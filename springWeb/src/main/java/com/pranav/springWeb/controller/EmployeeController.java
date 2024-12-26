03d7d05bad7a2cdef36977a3d2f38d78a8c8ef21
pranav-77 <pranavshazz77@gmail.com>
2024-12-26 21:56:18 +0530

    added new methods

--- springWeb/src/main/java/com/pranav/springWeb/controller/EmployeeController.java
+++ springWeb/src/main/java/com/pranav/springWeb/controller/EmployeeController.java
@@ -1,12 +1,22 @@
 package com.pranav.springWeb.controller;
 
+import com.pranav.springWeb.model.Student;
+import com.pranav.springWeb.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employee")
+@Slf4j
 public class EmployeeController {
     @PostMapping("/details/{name}")
     public String details(@PathVariable String name, @RequestParam String msg) {
         return name + " " + msg;
+    }
+
+    @PostMapping("/info")
+    public String allDetails(@RequestBody User user) {
+        log.info("All Details");
+        return user.getUserName();
     }
 }
